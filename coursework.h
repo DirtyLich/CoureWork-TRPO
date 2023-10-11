ae5c1144749177004a360327e64f90cb1ed59d77
playsees <124029605+playsees@users.noreply.github.com>
2023-10-11 20:54:20 +0700

    -fact

--- coursework.h
+++ coursework.h
@@ -15,4 +15,3 @@ int convertHOUR(float value, char choice);
 int convertDAY(float value, char choice);
 int convertMONTH(float value, char choice);
 int convertYEAR(float value, char choice);
-блин препод по ТРПО такой классный))
