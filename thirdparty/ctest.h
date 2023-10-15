d72d73424bc1c138336f83cd0cf2c9d9ea6ee1d2
playsees <124029605+playsees@users.noreply.github.com>
2023-10-15 20:16:19 +0700

    Update ctest.h

--- thirdparty/ctest.h
+++ thirdparty/ctest.h
@@ -30,7 +30,7 @@ union ctest_run_func_union {
 #if defined(__GNUC__)
 #if defined(__clang__) || __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 /* the GCC argument will work for both gcc and clang  */
-#define CTEST_IMPL_DIAG_PUSH_IGNORED(-who-) \
+#define CTEST_IMPL_DIAG_PUSH_IGNORED(who) \
     CTEST_IMPL_PRAGMA(GCC diagnostic push) \
     CTEST_IMPL_PRAGMA(GCC diagnostic ignored "-W" #w)
 #define CTEST_IMPL_DIAG_POP() \
