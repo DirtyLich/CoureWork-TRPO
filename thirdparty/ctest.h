68f5d725cc19a69ee3602362bd9444ad762414ec
playsees <124029605+playsees@users.noreply.github.com>
2023-10-15 19:10:30 +0700

    Update ctest.h

--- thirdparty/ctest.h
+++ thirdparty/ctest.h
@@ -1,18 +1,3 @@
-/* Copyright 2011-2023 Bas van den Berg
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #ifndef CTEST_H
 #define CTEST_H
 
@@ -40,12 +25,13 @@ union ctest_run_func_union {
     ctest_unary_run_func unary;
 };
 
-#define CTEST_IMPL_PRAGMA (x)__Pragma (#x)
+#define CTEST_IMPL_PRAGMA(x) _Pragma (#x)
 
 #if defined(__GNUC__)
 #if defined(__clang__) || __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 /* the GCC argument will work for both gcc and clang  */
 #define CTEST_IMPL_DIAG_PUSH_IGNORED(w) \
+    CTEST_IMPL_PRAGMA(GCC diagnostic push) \
     CTEST_IMPL_PRAGMA(GCC diagnostic ignored "-W" #w)
 #define CTEST_IMPL_DIAG_POP() \
     CTEST_IMPL_PRAGMA(GCC diagnostic pop)
